9ea73d4eb26fb5277b5c3e45b1c99d1bcc9096b2
ray73444 <ray73444@gmail.com>
2023-10-05 23:26:20 -0400

    ecommerce v1

---
+++ terraform.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+touch variables.tf
+touch outputs.tf
+touch providers.tf
+touch ec2.tf
+touch vpc.tf
+touch security_groups.tf
+touch load_balancer.tf
+touch iam.tf
+touch nacls.tf
+touch s3.tf
+touch efs.tf
+touch transit_gateway.tf
+touch bastion_host.tf
+touch autoscaling.tf
+
+
+echo "All files have been created in the E-COMMERCE-FORTRESS"
